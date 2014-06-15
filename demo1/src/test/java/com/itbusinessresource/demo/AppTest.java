30f73bce5c14253689aa8a53cf6667adfb50f9db
Darren Yeats <darrenyeats@gmail.com>
2014-06-15 18:45:11 +0100

    Updated comment

--- demo1/src/test/java/com/itbusinessresource/demo/AppTest.java
+++ demo1/src/test/java/com/itbusinessresource/demo/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App. Made with archetype:create ...
  */
 public class AppTest 
     extends TestCase
